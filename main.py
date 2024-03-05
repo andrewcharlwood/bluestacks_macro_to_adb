891ebe8059af8dd1745803217fcde8a90ae8eb3d
Andrew Charlwood <andrew.charlwood@gmail.com>
2024-03-05 10:05:26 +0000

    this isn't very good.

--- main.py
+++ main.py
@@ -1,16 +1,51 @@
-# This is a sample Python script.
+import argparse
+import json
+import shutil
+import os
+import pandas as pd
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import win32api
+import win32con
+import win32gui
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def click(x, y):
+    hwnd = win32gui.FindWindow(None, 'BlueStacks App Player')
+    lParam = win32api.MAKELONG(x, y)
+    hWnd1 = win32gui.FindWindowEx(hwnd, None, None, None)
+    win32gui.SendMessage(hWnd1, win32con.WM_LBUTTONDOWN, win32con.MK_LBUTTON, lParam)
+    win32gui.SendMessage(hWnd1, win32con.WM_LBUTTONUP, None, lParam)
+    return
+
+
+def copy_and_rename(src_path, dest_path, new_name):
+    # Copy the file
+    shutil.copy(src_path, dest_path)
+    # Rename the copied file
+    new_path = f"{dest_path}/{new_name}"
+    shutil.move(f"{dest_path}/{src_path}", new_path)
+
+
+def read_json(file):
+    with open(file) as f:
+        events = json.load(f)["Events"]
+    return events
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    parser = argparse.ArgumentParser(prog='Bluestacks to python ADB script convertor',
+                                    description='What it says on the tin',
+                                    epilog='Text at the bottom of help')
+    parser.add_argument('filename')
+    args = parser.parse_args()
+    macro_json = args.filename
+    copy_and_rename(os.path.join(os.getcwd(), "macro.py"), os.getcwd(), macro_json.replace(".json", ".py"))
+    #json_dict = read_json(macro_json)
+    data = json.loads(macro_json)
+    df = pd.json_normalize(data['results'])
+
+
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
